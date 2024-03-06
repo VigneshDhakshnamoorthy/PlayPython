efd4a2ac321b88995531f67f5f2d9ce86dea4254
VigneshDhakshnamoorthy <vigneshdhakshnamoorthy@gmail.com>
2024-03-07 00:39:56 +0530

    init

--- test_sample.py
+++ test_sample.py
@@ -1,21 +1,31 @@
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, Page
 import pytest
 
 @pytest.fixture(autouse=True)
-def page():
+def page() -> Page: # type: ignore
     with sync_playwright() as play:
-        browser = play.chromium.launch(headless=False)
-        page = browser.new_page()
+        browser = play.firefox.launch(args=['--start-maximized'], headless=False)
+        page = browser.new_page(no_viewport=True)
+        page
         yield page
         page.close()
     
-def test_google_1(page):
+def test_google_1(page:Page):
     page.goto("https://www.google.com")
     print("Chrome browser is opened")
+    search_box  = page.locator(selector="//*[@name='q']")
+    search_box.fill("Vignesh Dhakshnamoorthy")
+    search_box.press("Enter")
+    print(page.title())
+    page.wait_for_timeout(3000)
+    page.go_back()
+    search_box.clear()
+    search_box.fill("Vignesh Dhesna")
+    search_box.press("Enter")
     print(page.title())
     page.wait_for_timeout(3000)
 
-def test_google_2(page):
+def test_google_2(page:Page):
     page.goto("https://www.bing.com")
     print("Chrome browser is opened")
     print(page.title())
